1d8146975a5352eb747abd03497b65b4267f37a0
lloyd konneker <bootch@Peters-iMac.local>
2015-06-07 09:50:44 -0400

    Initial Commit

---
+++ Score/Score.h
@@ -0,0 +1,19 @@
+//
+//  Score.h
+//  Score
+//
+//  Created by lloyd konneker on 6/7/15.
+//  Copyright (c) 2015 lloyd konneker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Score.
+FOUNDATION_EXPORT double ScoreVersionNumber;
+
+//! Project version string for Score.
+FOUNDATION_EXPORT const unsigned char ScoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Score/PublicHeader.h>
+
+
